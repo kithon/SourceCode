21ae12465412be5f73621d5f45d7c3bb3ce53357
iwamoto <kithon@iwamotoPC.(none)>
2014-12-11 12:17:31 +0900

    first decision tree

---
+++ Decision/tree.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def DecisionTree(object):
+    def __init__(self):
+        print "init"
+
+    def entropy(self):
+        # get entropy
+        print "entropy"
+
+    def fit(self):
+        print "fit"
+
+    def predict(self):
+        print "predict"
+
+if __name__ == '__main__':
+    print "decision"
