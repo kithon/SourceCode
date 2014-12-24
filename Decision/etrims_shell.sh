5a86a12465ce2bab714adf3ada981268178b954c
iwamoto <iwamoto@laboratory.(none)>
2014-12-24 13:41:10 +0900

    for german coding

---
+++ Decision/etrims_shell.sh
@@ -0,0 +1,23 @@
+#/bin/zh
+
+# size = 3
+python tree2etrims.py 3 > test_result/etrims_size_3_1.log
+python tree2etrims.py 3 > test_result/etrims_size_3_2.log
+python tree2etrims.py 3 > test_result/etrims_size_3_3.log
+python tree2etrims.py 3 > test_result/etrims_size_3_4.log
+python tree2etrims.py 3 > test_result/etrims_size_3_5.log
+
+# size = 6
+python tree2etrims.py 6 > test_result/etrims_size_6_1.log
+python tree2etrims.py 6 > test_result/etrims_size_6_2.log
+python tree2etrims.py 6 > test_result/etrims_size_6_3.log
+python tree2etrims.py 6 > test_result/etrims_size_6_4.log
+python tree2etrims.py 6 > test_result/etrims_size_6_5.log
+
+# size = 9
+python tree2etrims.py 9 > test_result/etrims_size_9_1.log
+python tree2etrims.py 9 > test_result/etrims_size_9_2.log
+python tree2etrims.py 9 > test_result/etrims_size_9_3.log
+python tree2etrims.py 9 > test_result/etrims_size_9_4.log
+python tree2etrims.py 9 > test_result/etrims_size_9_5.log
+
--- Decision/tree2etrims.py
+++ Decision/tree2etrims.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-import scipy.io
 import datetime
 import numpy as np
 from PIL import Image
@@ -9,9 +8,9 @@ from tree import DecisionTree, ExtremeDecisionTree
 
 def print_time(message):
     d = datetime.datetime.today()
-    print '%s/%s/%s %s:%s:%s %s' % (d.year, d.month, d.day, d.hour, d.minute, d.second, message)    
+    print '%s/%s/%s %s:%s:%s.%s %s' % (d.year, d.month, d.day, d.hour, d.minute, d.second, d.microsecond, message)    
     
-def save_etrims(is08=True):
+def load_etrims(is08=True, size=6, shuffle=True, visualize=True):
     root_path = '../Dataset/etrims-db_v1/'
     an_name = 'annotations/'
     im_name = 'images/'
@@ -22,7 +21,8 @@ def save_etrims(is08=True):
     # annotations part
     path = root_path + an_name + et_name
     dir_list = os.listdir(path)
-    signal = []
+    mat_signal = []
+
     print "annotations..."
     for file_name in dir_list:
         print file_name
@@ -31,17 +31,19 @@ def save_etrims(is08=True):
 
         # get width and height
         w, h = image.size
-        image_signal = [[0] * w] * h
-        for i in xrange(w * h):
-            x, y = i % w, i / w
-            image_signal[y][x] = image.getpixel((x, y))
-        signal.append(image_signal)
-    
+        image_signal = []
+        for y in xrange(h):
+            row = []
+            for x in xrange(w):
+                row.append(image.getpixel((x, y)))
+            image_signal.append(row)
+        mat_signal.append(image_signal)
+
         
     # images part
     path = root_path + im_name + et_name
     dir_list = os.listdir(path)
-    data = []
+    mat_data = []
     print "images..."
     for file_name in dir_list:
         print file_name
@@ -51,43 +53,16 @@ def save_etrims(is08=True):
 
         # get width and height
         w, h = image.size
-        image_data = [[0] * w] * h
-        for i in xrange(w * h):
-            x, y = i % w, i / w
-            image_data[y][x] = list(image.getpixel((x,y)))
-        data.append(image_data)
+        image_data = []
+        for y in xrange(h):
+            row = []
+            for x in xrange(w):
+                row.append(list(image.getpixel((x,y))))
+            image_data.append(row)
+        mat_data.append(image_data)
+        
 
-    dir_name = 'etrims_mat/'
-    file_name = 'etrims_08.mat' if is08 else 'etrims_04.mat'
-
-    mat = {'signal': signal, 'data': data, 'name': dir_list}
-    scipy.io.savemat(dir_name + file_name, mat)
-    print "save mat: done"
-
-def load_etrims(is08=True, size=6, shuffle=True, OVERWRITE=False, visualize=True):
-    print "Searching",
-    dir_name = 'etrims_mat/'
-    file_name = 'etrims_08.mat' if is08 else 'etrims_04.mat'
-    path = dir_name + file_name
-    print path, "..."
-
-    if not os.path.exists(path):
-        print path, "does not exist"
-        print "making mat data"
-        if not os.path.exists(dir_name):
-            os.system('mkdir ' + dir_name)
-        save_etrims(is08)
-    elif OVERWRITE:
-        print "OVERWRITE is True"
-        print "making forcely mat data"
-        save_etrims(is08)
-
-    # scipy.io.load mat file
-    print "load mat file"    
-    mat = scipy.io.loadmat(path)
-    mat_signal = mat['signal']
-    mat_data = mat['data']
-
+        
     # train index
     train_index = []
     DATA_SIZE = 60
@@ -107,40 +82,62 @@ def load_etrims(is08=True, size=6, shuffle=True, OVERWRITE=False, visualize=True
     # divide train and test
     test_or_train = [[], []]
     train_data, train_signal, test_data, test_signal = [],[],[],[]
-    for i in xrange(DATA_SIZE):
-        signal_w_h = mat_signal[0,i]
-        data_w_h = mat_data[0,i]
+    for iteration in xrange(DATA_SIZE):
+        signal_w_h = mat_signal[iteration]
+        data_w_h = mat_data[iteration]
         width = len(signal_w_h) - 1
         height = len(signal_w_h[0]) - 1
 
         signal_set = [test_signal, train_signal]
         data_set   = [test_data,   train_data]
-        index = i in train_index
+        index = iteration in train_index
 
         # information
-        test_or_train[index].append(i)
+        test_or_train[index].append(iteration)
 
         # visualize
         if visualize:
-            sys.stdout.write("\r %dth data train:%d test:%d labeling %.1f%%" % (i, len(test_or_train[1]), len(test_or_train[0]), (100.*i/DATA_SIZE)))
+            sys.stdout.write("\r %dth data train:%d test:%d labeling %.1f%%" % (iteration, len(test_or_train[1]), len(test_or_train[0]), (100.*iteration/DATA_SIZE)))
             sys.stdout.flush()
 
         for x,row in enumerate(signal_w_h):    
             for y,element in enumerate(row):
+                #print x,y
                 # signal append
                 signal = element
                 signal_set[index].append(signal)
 
                 # data append
-                x1, x2 = max(0, x-size), min(width, x+size)
-                y1, y2 = max(0, y-size), min(height, y+size)
-                crop = data_w_h[x1:x2][y1:y2]
-                data = [[0,0,0] * (2*size+1)] * (2*size+1)
-                data_x1, data_x2 = x1-(x-size), 2*width-((x+size)-x2)
-                data_y1, data_y2 = y1-(y-size), 2*height-((y+size)-y2)
-                data[data_x1:data_x2][data_y1:data_y2] = crop
+                x1, x2 = max(0, x-size), min(width, x+(size+1))
+                y1, y2 = max(0, y-size), min(height, y+(size+1))
+                crop = data_w_h[x1:x2]
+
+                temp = []
+                for i in xrange(len(crop)):
+                    temp.append(crop[i][y1:y2])
+                crop = temp
+
+                data = []
+                for i in xrange(2*size+1):
+                    row = []
+                    for j in xrange(2*size+1):
+                        row.append([0,0,0])
+                    data.append(row)                
+                
+                data_x1, data_y1 = x1-(x-size), y1-(y-size)
+                data_x2 = data_x1 + x2 - x1
+                data_y2 = data_y1 + y2 - y1
+                for i in xrange(data_x1, data_x2):
+                    for j in xrange(data_y1, data_y2):
+                        if iteration == 1:
+                            print i,j,len(crop),len(crop[0]), data_x1, data_x2, data_y1, data_y2, x1, x2, y1, y2
+                        data[i][j] = crop[i-data_x1][j-data_y1]
+
+                #print data, len(data), len(data[0])
+                    
                 data = [flatten for inner in data for flatten in inner]
                 data = [flatten for inner in data for flatten in inner]
+                #print "one", data
                 data_set[index].append(data)
 
     print " done."
@@ -153,7 +150,7 @@ def etrims_tree(n_hidden = [1000], coef = [1000.], size=6):
     print_time('load_etrims')
     train_data, train_signal, test_data, test_signal = load_etrims(size=size)
 
-    num_function = 50
+    num_function = 100
     print_time('train_DecisionTree num function is %d' % num_function)
     dt = DecisionTree(num_function=num_function)
     dt.fit(train_data, train_signal)
