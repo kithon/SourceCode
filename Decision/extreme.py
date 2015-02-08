294be2f0a9e1b958bd52443f106da83e55ed09ff
iwamoto <kithon@iwamotoPC.(none)>
2015-02-08 22:09:00 +0900

    binary extreme

--- Decision/extreme.py
+++ Decision/extreme.py
@@ -313,6 +313,122 @@ class ELMAutoEncoder(object):
 
     
 ##########################################################
+##  Binary Extreme Learning Machine Classifier
+##########################################################
+    
+class BinaryELMClassifier(object):
+    """
+    ELMClassifier : 
+        __init__ : 
+            activation : Layer's activation
+            vector : Policy of generating Layers weight ('orthogonal' or 'random')
+            n_hidden : Hidden Layer's number of neuron
+            coef : coefficient for Layer's ridge redression
+            seed : seed for np.random.RandomState
+            domain : domain for initial value of weight and bias
+    """
+
+    def __init__(self, activation=sigmoid, vector='random',
+                 n_hidden=50, coef=None, seed=123, domain=[-1., 1.]):
+        self.activation = activation
+        self.vector = vector
+        self.coef = coef
+        self.n_hidden = n_hidden
+        self.np_rng = np.random.RandomState(seed)
+        self.domain = domain
+        
+    def get_weight(self):
+        return self.weight
+
+    def get_bias(self):
+        return self.bias
+
+    def get_beta(self):
+        return self.layer.beta
+
+    def fit(self, input, teacher):
+        # set input, teacher and class
+        self.input = input
+        self.teacher = teacher
+        self.n_input = len(input[0])
+        self.n_output = len(self.classes)
+
+        # weight and bias
+        low, high = self.domain
+        weight = self.np_rng.uniform(low = low,
+                                     high = high,
+                                     size = (self.n_input,
+                                             self.n_hidden))
+        bias = self.np_rng.uniform(low = low,
+                                   high = high,
+                                   size = self.n_hidden)
+
+        # condition : orthogonal or random
+        if self.vector == 'orthogonal':
+            # orthogonalize weight
+            for i in xrange(len(weight)):
+                w = weight[i]
+                for j in xrange(0, i):
+                    w = w - weight[j].dot(w) * weight[j]
+                w = w / np.linalg.norm(w)
+                weight[i] = w
+
+            # regularize bias
+            denom = np.linalg.norm(bias)
+            if denom != 0:
+                denom = bias / denom
+                    
+        elif self.vector == 'random':
+            # regularize weight
+            for i,w in enumerate(weight):
+                denom = np.linalg.norm(w)
+                if denom != 0:
+                    weight[i] = w / denom
+
+            # regularize bias
+            denom = np.linalg.norm(bias)
+            if denom != 0:
+                bias = bias / denom
+                    
+        else:
+            print "warning: vector isn't orthogonal or random"
+            
+        
+        # generate self weight and bias
+        self.weight = weight
+        self.bias = bias
+            
+        # generate self layer
+        self.layer = Layer(self.activation,
+                           [self.n_input, self.n_hidden, self.n_output],
+                           self.weight,
+                           self.bias,
+                           self.coef)
+
+        # fit layer
+        self.layer.fit(input, teacher)
+        return self.get_weight(), self.get_bias(), self.get_beta()
+        
+    def predict(self, input):
+        # get predict_output
+        predict_output = []
+        for i in input:
+            o = self.layer.get_output(i).tolist()
+            predict_output.append(o)        #print "outputs", predict_output
+
+        return predict_output
+
+    def score(self, input, teacher):
+        # get score 
+        count = 0
+        length = len(teacher)
+        predict_classes = self.predict(input)
+        for i in xrange(length):
+            if predict_classes[i] == teacher[i]: count += 1
+        return count * 1.0 / length
+    
+    
+##########################################################
 ##  Extreme Learning Machine Classifier
 ##########################################################
     
@@ -328,7 +444,7 @@ class ELMClassifier(object):
             domain : domain for initial value of weight and bias
     """
 
-    def __init__(self, activation=sigmoid, vector='orthogonal',
+    def __init__(self, activation=sigmoid, vector='random',
                  n_hidden=50, coef=None, seed=123, domain=[-1., 1.]):
         self.activation = activation
         self.vector = vector
@@ -418,14 +534,14 @@ class ELMClassifier(object):
 
         # fit layer
         self.layer.fit(input, signal)
+        return self.get_weight(), self.get_bias(), self.get_beta()
         
     def predict(self, input):
         # get predict_output
         predict_output = []
         for i in input:
             o = self.layer.get_output(i).tolist()
-            predict_output.append(o)
-        #print "outputs", predict_output
+            predict_output.append(o)        #print "outputs", predict_output
 
         # get predict_classes from index of max_function(predict_output) 
         predict_classes = []
