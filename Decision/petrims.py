7e164fa4002d7bd1b498a97e026349c50d8ab427
iwamoto <kithon@iwamotoPC.(none)>
2015-02-13 17:15:14 +0900

    overlap2

--- Decision/petrims.py
+++ Decision/petrims.py
@@ -763,7 +763,7 @@ def etrims_tree(radius, size, d_limit, unshuffle, four, num, parameter, t_args, 
         dt = DecisionTree(radius=radius, num_function=num, file_name=file_name)
         
         print_time('DecisionTree overlap: train', file_name)
-        dt.fit(train_set, d_limit=d_limit, overlap=False)
+        dt.fit(train_set, d_limit=d_limit, overlap=True)
         
         print_time('DecisionTree overlap: test', file_name)
         score = dt.score(test_set)
