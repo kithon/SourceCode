c096b3f3eb6d5da1674d459c14f4a7a9ed41e4f8
iwamoto <kithon@iwamotoPC.(none)>
2015-02-13 10:31:36 +0900

    revise shell

--- Decision/etrims_andante.sh
+++ Decision/etrims_andante.sh
@@ -1,13 +1,13 @@
 #/bin/sh
 
 # andante
-python etrims_test.py test_andante/result_1.log 2> test_andante/error_1.log
-python etrims_test.py test_andante/result_2.log 2> test_andante/error_2.log
-python etrims_test.py test_andante/result_3.log 2> test_andante/error_3.log
-python etrims_test.py test_andante/result_4.log 2> test_andante/error_4.log
-python etrims_test.py test_andante/result_5.log 2> test_andante/error_5.log
-python etrims_test.py test_andante/result_6.log 2> test_andante/error_6.log
-python etrims_test.py test_andante/result_7.log 2> test_andante/error_7.log
-python etrims_test.py test_andante/result_8.log 2> test_andante/error_8.log
-python etrims_test.py test_andante/result_9.log 2> test_andante/error_9.log
-python etrims_test.py test_andante/result_10.log 2> test_andante/error_10.log
+python etrims.py test_andante/result_1.log 2> test_andante/error_1.log
+python etrims.py test_andante/result_2.log 2> test_andante/error_2.log
+python etrims.py test_andante/result_3.log 2> test_andante/error_3.log
+python etrims.py test_andante/result_4.log 2> test_andante/error_4.log
+python etrims.py test_andante/result_5.log 2> test_andante/error_5.log
+python etrims.py test_andante/result_6.log 2> test_andante/error_6.log
+python etrims.py test_andante/result_7.log 2> test_andante/error_7.log
+python etrims.py test_andante/result_8.log 2> test_andante/error_8.log
+python etrims.py test_andante/result_9.log 2> test_andante/error_9.log
+python etrims.py test_andante/result_10.log 2> test_andante/error_10.log
--- Decision/etrims_dacapo.sh
+++ Decision/etrims_dacapo.sh
@@ -1,13 +1,13 @@
 #/bin/sh
 
 # dacapo
-python etrims_test.py test_dacapo/result_1.log 2> test_dacapo/error_1.log
-python etrims_test.py test_dacapo/result_2.log 2> test_dacapo/error_2.log
-python etrims_test.py test_dacapo/result_3.log 2> test_dacapo/error_3.log
-python etrims_test.py test_dacapo/result_4.log 2> test_dacapo/error_4.log
-python etrims_test.py test_dacapo/result_5.log 2> test_dacapo/error_5.log
-python etrims_test.py test_dacapo/result_6.log 2> test_dacapo/error_6.log
-python etrims_test.py test_dacapo/result_7.log 2> test_dacapo/error_7.log
-python etrims_test.py test_dacapo/result_8.log 2> test_dacapo/error_8.log
-python etrims_test.py test_dacapo/result_9.log 2> test_dacapo/error_9.log
-python etrims_test.py test_dacapo/result_10.log 2> test_dacapo/error_10.log
+python etrims.py test_dacapo/result_1.log 2> test_dacapo/error_1.log
+python etrims.py test_dacapo/result_2.log 2> test_dacapo/error_2.log
+python etrims.py test_dacapo/result_3.log 2> test_dacapo/error_3.log
+python etrims.py test_dacapo/result_4.log 2> test_dacapo/error_4.log
+python etrims.py test_dacapo/result_5.log 2> test_dacapo/error_5.log
+python etrims.py test_dacapo/result_6.log 2> test_dacapo/error_6.log
+python etrims.py test_dacapo/result_7.log 2> test_dacapo/error_7.log
+python etrims.py test_dacapo/result_8.log 2> test_dacapo/error_8.log
+python etrims.py test_dacapo/result_9.log 2> test_dacapo/error_9.log
+python etrims.py test_dacapo/result_10.log 2> test_dacapo/error_10.log
