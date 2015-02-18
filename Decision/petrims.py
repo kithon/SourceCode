f6abc308a31f61d3ffeb82a46ac57635155dbd32
iwamoto <kithon@iwamotoPC.(none)>
2015-02-18 10:51:07 +0900

    sava load

--- Decision/petrims.py
+++ Decision/petrims.py
@@ -878,6 +878,7 @@ def etrims_tree_makeup(radius, size, d_limit, unshuffle, cram, four, num, parame
     param = literal_eval(linecache.getline(parameter, 1))
     radius, size, d_limit, unshuffle, four, num = param[0:6]
     t_index = literal_eval(linecache.getline(parameter, 4+size))
+    print t_index
     train_set, test_set = load_etrims(radius=radius, size=size, is08=not four, shuffle=not unshuffle, name=file_name, t_index=t_index)
     isDT, isEDT, isREDT, isBEDT = t_args
     
