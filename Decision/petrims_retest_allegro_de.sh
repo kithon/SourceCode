7b8c8ec2c49762c742d22b8a3615084ac81909f6
iwamoto <kithon@iwamotoPC.(none)>
2015-02-19 00:05:15 +0900

    retest

---
+++ Decision/petrims_retest_allegro_de.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+# allegro
+python petrims.py -c -t de test_allegro/result_21.log 2 2> test_allegro/error_21.log
+python petrims.py -c -t de test_allegro/result_22.log 2 2> test_allegro/error_22.log
+python petrims.py -c -t de test_allegro/result_23.log 2 2> test_allegro/error_23.log
+python petrims.py -c -t de test_allegro/result_24.log 2 2> test_allegro/error_24.log
+python petrims.py -c -t de test_allegro/result_25.log 2 2> test_allegro/error_25.log
+python petrims.py -c -t de test_allegro/result_26.log 2 2> test_allegro/error_26.log
+python petrims.py -c -t de test_allegro/result_27.log 2 2> test_allegro/error_27.log
+python petrims.py -c -t de test_allegro/result_28.log 2 2> test_allegro/error_28.log
+python petrims.py -c -t de test_allegro/result_29.log 2 2> test_allegro/error_29.log
+python petrims.py -c -t de test_allegro/result_30.log 2 2> test_allegro/error_30.log
---
+++ Decision/petrims_retest_andante_de.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+# andante
+python petrims.py -c -t de test_andante/result_21.log 2 2> test_andante/error_21.log
+python petrims.py -c -t de test_andante/result_22.log 2 2> test_andante/error_22.log
+python petrims.py -c -t de test_andante/result_23.log 2 2> test_andante/error_23.log
+python petrims.py -c -t de test_andante/result_24.log 2 2> test_andante/error_24.log
+python petrims.py -c -t de test_andante/result_25.log 2 2> test_andante/error_25.log
+python petrims.py -c -t de test_andante/result_26.log 2 2> test_andante/error_26.log
+python petrims.py -c -t de test_andante/result_27.log 2 2> test_andante/error_27.log
+python petrims.py -c -t de test_andante/result_28.log 2 2> test_andante/error_28.log
+python petrims.py -c -t de test_andante/result_29.log 2 2> test_andante/error_29.log
+python petrims.py -c -t de test_andante/result_30.log 2 2> test_andante/error_30.log
