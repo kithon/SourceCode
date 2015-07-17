6e0834d0fc56e91613e46aaa1ef5090a1ffd1404
iwamoto <kithon@iwamotoPC.(none)>
2015-07-17 15:49:35 +0900

    debug

--- Gbdt/gradient_boosting.py
+++ Gbdt/gradient_boosting.py
@@ -92,6 +92,7 @@ def predict_pixel(hist, picture, fileName):
             for k in xrange(height):
                 label = picture[i].getSignal(j,k)
                 # predict & count
+                print_time(hist[i,j,k], fileName)
                 predict[i,j,k] = max(hist[i,j,k].iteritems(), key=operator.itemgetter(1))[0]
                 if predict[i,j,k] == label:
                     one_TP += 1
